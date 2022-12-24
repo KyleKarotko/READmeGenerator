4eb5dfafccdc1d20ffd29826de617e5e2d5f232b
kyle_karotko <kkrtk7@duck.com>
2022-12-23 17:28:18 -0800

    added function to generate readme layout

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generate = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
+const makeReadMe = ({title, description, installation, usage, license, contributing, tests, questions}) =>
+`#${title}
+####${description}
+####${installation}
+####${usage}
+####${license}
+####${contributing}
+####${tests}
+####${questions}`;
+
+
+
+
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
